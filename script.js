2b6d9277ac347fc6def1c3cb602289a199ce6c0f
Priti Munde <120581202+pritimunde1999@users.noreply.github.com>
2023-07-20 12:57:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 
+let yourArray = ["hello", true, 10, 20, false];
+let complexArray = [
+  [
+    {
+      one: 1,
+      two: 2
+    },
+    {
+      three: 3,
+      four: 4
+    }
+  ],
+  [
+    {
+      a: "a",
+      b: "b"
+    },
+    {
+      c: "c",
+      d: "d"
+    }
+  ]
+];
+
+console.log(complexArray.length);
